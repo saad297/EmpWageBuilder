91903308a2db23aed317473a074873574dd54c8d
Saad <skamil60@gmail.com>
2020-07-11 16:54:39 +0530

    Added : Condition For Wage Calculation

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -4,14 +4,18 @@ class EmpWageBuilder
         public static final int IS_PART_TIME=2;
         public static final int EMP_RATE_PER_HOUR=20;
 	public static final int NUM_OF_WORKING_DAYS=20;
+	public static final int MAX_WORK_HOURS=100;
 
 	public static void main(String[] args)
 	{
 		int empHrs=0;
 		int empWage=0;
 		int totalEmpWage=0;
-		for (int day=0; day < NUM_OF_WORKING_DAYS;day++)
+		int totalEmpHrs=0;
+		int days=0;
+		while (empHrs <= MAX_WORK_HOURS && days < NUM_OF_WORKING_DAYS)
 		{
+			days++;
 			int empCheck=(int)Math.floor(Math.random() * 10) %3;
 			switch (empCheck) {
 				case IS_FULL_TIME:
@@ -23,10 +27,10 @@ class EmpWageBuilder
 				default:
 					empHrs=0;
 			}
-			empWage=EMP_RATE_PER_HOUR*empHrs;
-			totalEmpWage += empWage;
-			System.out.println("The daily employee wage is : " + empWage);
+			totalEmpHrs += empHrs;
+			System.out.println("Total days : " + days + "	Total hours are : " + totalEmpHrs);
 		}
+		totalEmpWage=totalEmpHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Total employee wage is : " + totalEmpWage);
 	}
 }
