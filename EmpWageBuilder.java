526118d946d5ced7a7d7a2281c797af6cb6fd7e3
Saad <skamil60@gmail.com>
2020-07-13 03:05:33 +0530

    Added : Function to compute employee wage

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -6,31 +6,36 @@ class EmpWageBuilder
 	public static final int NUM_OF_WORKING_DAYS=20;
 	public static final int MAX_WORK_HOURS=100;
 
+	public static int computeEmpWage() {
+		int empHrs=0;
+              	int empWage=0;
+                int totalEmpWage=0;
+                int totalEmpHrs=0;
+                int days=0;
+                while (empHrs <= MAX_WORK_HOURS && days < NUM_OF_WORKING_DAYS)
+                {
+                        days++;
+                        int empCheck=(int)Math.floor(Math.random() * 10) %3;
+                        switch (empCheck) {
+                                case IS_FULL_TIME:
+                                        empHrs=8;
+                        break;
+                                case IS_PART_TIME:
+                                        empHrs=4;
+                        break;
+                                default:
+                                        empHrs=0;
+                        }
+                        totalEmpHrs += empHrs;
+                        System.out.println("Total days : " + days + "   Total hours are : " + totalEmpHrs);
+                }
+                totalEmpWage=totalEmpHrs * EMP_RATE_PER_HOUR;
+                System.out.println("Total employee wage is : " + totalEmpWage);
+		return totalEmpWage;
+	}
+
 	public static void main(String[] args)
 	{
-		int empHrs=0;
-		int empWage=0;
-		int totalEmpWage=0;
-		int totalEmpHrs=0;
-		int days=0;
-		while (empHrs <= MAX_WORK_HOURS && days < NUM_OF_WORKING_DAYS)
-		{
-			days++;
-			int empCheck=(int)Math.floor(Math.random() * 10) %3;
-			switch (empCheck) {
-				case IS_FULL_TIME:
-					empHrs=8;
-			break;
-				case IS_PART_TIME:
-					empHrs=4;
-			break;
-				default:
-					empHrs=0;
-			}
-			totalEmpHrs += empHrs;
-			System.out.println("Total days : " + days + "	Total hours are : " + totalEmpHrs);
-		}
-		totalEmpWage=totalEmpHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Total employee wage is : " + totalEmpWage);
+		computeEmpWage();
 	}
 }
