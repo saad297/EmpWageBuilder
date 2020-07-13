730dd6bb68b83c129adb408c760de1313f8cc5b6
Saad <skamil60@gmail.com>
2020-07-14 04:00:33 +0530

    Changes made : removed total emp wage part

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -3,10 +3,9 @@ class EmpWageBuilder
 	public static final int IS_FULL_TIME=1;
         public static final int IS_PART_TIME=2;
 
-	public static int computeEmpWage(String company, int empRate, int numOfDays, int maxHrs) {
+	public static void computeEmpWage(String company, int empRate, int numOfDays, int maxHrs) {
 		int empHrs=0;
               	int empWage=0;
-                int totalEmpWage=0;
                 int totalEmpHrs=0;
                 int days=0;
                 while (empHrs <= maxHrs && days < numOfDays)
@@ -26,9 +25,6 @@ class EmpWageBuilder
                         totalEmpHrs += empHrs;
                         System.out.println("Total days : " + days + "   Total hours are : " + totalEmpHrs);
                 }
-                totalEmpWage=totalEmpHrs * empRate;
-                System.out.println("Total employee wage in "+company+" is : " + totalEmpWage);
-		return totalEmpWage;
 	}
 
 	public static void main(String[] args)
